5f9dbfea80850bc3847738535ff1dac6018e1760
davidvergara <david13z@hotmail.com>
2015-10-17 14:07:15 +0200

    Comments added to Objective 2 code

--- GrandRepositoryInTheSky/src/dht/receive.go
+++ GrandRepositoryInTheSky/src/dht/receive.go
@@ -9,6 +9,8 @@ import (
 	"strconv"
 )
 
+//Starts a go function that will be receiving messages
+//for a node all the time
 func (receive *DHTNode) StartListenServer(){
 	
 	addr, err := net.ResolveUDPAddr("udp",(":"+receive.GetPort()))
@@ -42,6 +44,8 @@ func (receive *DHTNode) StartListenServer(){
 	}()
 }
 
+//Decrypts a message received and calls the function that has 
+//to work with that message
 func (receive *DHTNode) decryptMessage (bytesReceived []byte){
 	var message Msg
 	err := json.Unmarshal(bytesReceived, &message)
@@ -59,10 +63,6 @@ func (receive *DHTNode) decryptMessage (bytesReceived []byte){
 		{
 			receive.receiveLookupAnswer(message)
 		}
-		case message.Type == "ADDTORING":
-		{
-			receive.receiveAddToRing(message)
-		}
 		case message.Type == "SETPREDECESSOR":
 		{
 			receive.receiveSetPredecessor(message)
@@ -78,6 +78,10 @@ func (receive *DHTNode) decryptMessage (bytesReceived []byte){
 		case message.Type == "PRINTRINGAUX":
 		{
 			receive.receivePrintRingAux(message)
+		}
+		case message.Type == "ADDTORING":
+		{
+			receive.receiveAddToRing(message)
 		}
 		case message.Type == "UPDATEFINGERTABLES":
 		{
@@ -97,49 +101,72 @@ func (receive *DHTNode) decryptMessage (bytesReceived []byte){
 		}
 	}
 }
-	
+
+//Received meesage 	"LOOKUP"
+//A Lookup function will be run with the arguments of the message
 func (receive *DHTNode) receiveLookup (message Msg){
 	receive.Lookup(message.Args["key"],message.Source, message.Args["lookUpId"]) 
 }
 
+//Received meesage 	"LOOKUPANSWER"
+//A answer to a Lookup has been received, saving that 
+//answer in the corresponding channel
 func (receive *DHTNode) receiveLookupAnswer (message Msg){
 	idLookup,_ := strconv.Atoi(message.Args["lookUpId"])
 	receive.LookupRequest[idLookup] <- message.Source
 }
 
+//Received message "SETPREDECESSOR"
+//Uploading predecessor of the node...
 func (receive *DHTNode) receiveSetPredecessor (message Msg){
 	newPredecessor := message.Source
 	receive.SetPredecessor(newPredecessor)
 }
 
+//Received message "SETSUCCESSOR"
+//Uploading successor of the node...
 func (receive *DHTNode) receiveSetSuccessor (message Msg){
 	newSuccessor := message.Source
 	receive.SetSuccessor(newSuccessor)
 }
 
+//Received message "PRINTRING"
+//Calling PrintRing function to print the ring
 func (receive *DHTNode) receivePrintRing(message Msg){
 	receive.PrintRing()
 }
 
+//Received message "PRINTRINGAUX"
+//Calling PrintRingAux function to print the ring
 func (receive *DHTNode) receivePrintRingAux(message Msg){
 	origin := message.Source
 	receive.PrintRingAux(origin)
 }
 
+//Received message "ADDTORING"
+//Calling AddToRing funtion to insert a node in the ring
 func (receive *DHTNode) receiveAddToRing(message Msg){
 	newNode := message.Source
 	receive.AddToRing(newNode)
 }
 
+//Received message "UPDATEFINGERTABLES"
+//Calling updateFingerTables to update the finger tables of the node
 func (receive *DHTNode) receiveUpdateFingerTables(message Msg){
 	receive.updateFingerTables()
 }
 
+//Received message "UPDATEFINGERTABLESAUX"
+//Calling updateFingerTablesAux function to update the finger tables
+//of the node
 func (receive *DHTNode) receiveUpdateFingerTablesAux(message Msg){
 	origin := message.Source
 	receive.updateFingerTablesAux(origin)
 }
 
+//Received message "INSERTNODEBEFOREME"
+//Calling InsertNodeBeforeMe function to insert a node before
+//the local node
 func (receive *DHTNode) receiveInsertNodeBeforeMe(message Msg){
 	nodeToInsert := message.Source
 	receive.InsertNodeBeforeMe(nodeToInsert)
